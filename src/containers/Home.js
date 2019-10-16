f8c04f79fdd576095879b7c397646d859e3bcd02
System Administrator <conary@hotmail.es>
2019-10-16 12:44:19 -0600

    Initial commit 😬

---
+++ src/containers/Home.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import CategoryList from '../components/CategoryList';
+
+export default class Home extends Component {
+  render() {
+    return (
+      <>
+        <CategoryList />
+      </>
+    );
+  }
+}
