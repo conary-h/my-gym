42be027411e85a00758df1f0e6294cbd8c086ced
Christopher Escalon <escalonchristopher@gmail.com>
2019-10-18 13:14:53 -0600

    🔥 styles

--- src/App.js
+++ src/App.js
@@ -2,11 +2,11 @@ import React from 'react';
 import { Router } from '@reach/router';
 import Dashboard from './containers/Dashboard';
 import CategoryDetail from './components/CategoryDetail';
-
-import './styles/main.scss';
-import 'flexboxgrid/css/flexboxgrid.min.css';
 import CategoryList from './components/CategoryList';
 import ExerciseDetail from './components/ExerciseDetail';
+
+import './styles/main.scss';
+import 'normalize.css/normalize.css';
 
 function App() {
   return (
--- src/components/MuscleItem.js
+++ src/components/MuscleItem.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import { Link } from '@reach/router';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faChevronRight } from '@fortawesome/free-solid-svg-icons';
 
 export default function MuscleItem({ muscleData: { id, muscle } }) {
   return (
     <Link to={`categories/${id}`}>
       <div className="card">
         <strong>{muscle}</strong>
+        <FontAwesomeIcon icon={faChevronRight} />
       </div>
     </Link>
   );
